c643341fca29afff54219f49d45747429f5ce82e
joeymariano <joey.michalina.mariano@gmail.com>
2020-06-05 02:10:37 -0400

    passing props from board to squares

--- src/components/board.js
+++ src/components/board.js
@@ -10,7 +10,18 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+        />
+    )
+  }
+  
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X'
+    this.setState({squares: squares})
   }
 
   render() {

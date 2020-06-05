6244b3be80335a7f24138ea6b78847605390034e
joeymariano <joey.michalina.mariano@gmail.com>
2020-06-04 22:22:53 -0400

    init

---
+++ src/components/square.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Component from 'react';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+export default Square

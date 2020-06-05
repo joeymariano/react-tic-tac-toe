ad40b93a4dae86c597a8345bb25f3f8c77c7684b
joeymariano <joey.michalina.mariano@gmail.com>
2020-06-05 01:58:06 -0400

    added constructor to board

--- src/components/board.js
+++ src/components/board.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Square from './square.js'
 
 class Board extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
   renderSquare(i) {
     return <Square value={i}/>;
   }
